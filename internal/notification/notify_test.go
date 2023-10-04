d532cbe0eee897cafe056922107892ea68240f70
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-10-05 00:07:28 +0900

    add: test

---
+++ internal/notification/notify_test.go
@@ -0,0 +1,48 @@
+package notification_test
+
+import (
+	"testing"
+
+	"github.com/yoshihiro-shu/financial-bot/internal/notification"
+)
+
+const accessToken = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+func TestSendMsg(t *testing.T) {
+	client := notification.NewNotification(accessToken)
+
+	tests := []struct {
+		name string
+		Args string
+		Want error
+	}{
+		{
+			name: "test",
+			Args: "test",
+			Want: nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := client.SendMsg(test.Args)
+			if err != test.Want {
+				t.Errorf("got %v, want %v", err, test.Want)
+			}
+		})
+	}
+}
+
+func FuzzSendMsg(f *testing.F) {
+	client := notification.NewNotification(accessToken)
+	testcases := []string{"Hello, world", " ", "!12345"}
+	for _, tc := range testcases {
+		f.Add(tc) // Use f.Add to provide a seed corpus
+	}
+	f.Fuzz(func(t *testing.T, msg string) {
+		err := client.SendMsg(msg)
+		if err != nil {
+			t.Error(err)
+		}
+	})
+}
