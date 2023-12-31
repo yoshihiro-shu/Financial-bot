24c29ed49a415c08fdfef0ea44bb937ac2a5cb05
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-12-31 19:43:21 +0900

    refactor

--- repository/apache_kafka/client.go
+++
@@ -1,7 +0,0 @@
-package kafka
-
-import "github.com/IBM/sarama"
-
-func NewConsumerClient(addrs []string, groupName string, config *sarama.Config) (sarama.ConsumerGroup, error) {
-	return sarama.NewConsumerGroup(addrs, groupName, config)
-}
