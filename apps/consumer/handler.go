15f8fa1554c3a0591160b4a72ac3173c19a08cff
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-12-31 21:42:40 +0900

    fix: consumer handler

--- apps/consumer/handler.go
+++ apps/consumer/handler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/IBM/sarama"
 )
@@ -14,9 +15,25 @@ func (consumerGroupHandler) Setup(_ sarama.ConsumerGroupSession) error   { retur
 func (consumerGroupHandler) Cleanup(_ sarama.ConsumerGroupSession) error { return nil }
 func (consumerGroupHandler) ConsumeClaim(sess sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	for msg := range claim.Messages() {
-		fmt.Printf("Message topic:%q partition:%d offset:%d\n", msg.Topic, msg.Partition, msg.Offset)
-		fmt.Printf("Message key:%s value:%s\n", msg.Key, msg.Value)
+		switch msg.Topic {
+		case "request":
+			handleRequest(msg)
+		default:
+			handleDefault(msg)
+		}
 		sess.MarkMessage(msg, "")
 	}
 	return nil
+}
+
+func handleRequest(msg *sarama.ConsumerMessage) {
+	switch string(msg.Key) {
+	case http.MethodGet:
+		http.Get(string(msg.Value))
+	}
+}
+
+func handleDefault(msg *sarama.ConsumerMessage) {
+	fmt.Printf("Message topic:%q partition:%d offset:%d\n", msg.Topic, msg.Partition, msg.Offset)
+	fmt.Printf("Message key:%s value:%s\n", msg.Key, msg.Value)
 }
