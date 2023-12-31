219e94f6c5e01d9ab6d02eefb3381d6b1ab00d25
yoshihiro-shu <yoihongzhu0321@gmail.com>
2024-01-01 00:02:29 +0900

    add:get  symbol list

---
+++ internal/fmp/symbol_list.go
@@ -0,0 +1,41 @@
+package fmp
+
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/goccy/go-json"
+)
+
+type symbol struct {
+	Symbol  string  `json:"symbol"`
+	Name    string  `json:"name"`
+	Price   float32 `json:"price"`
+	Ex      string  `json:"exchange"`
+	ExShort string  `json:"exchangeShortName"`
+	Type    string  `json:"type"`
+}
+
+// A list of all traded and non-traded stocks.
+// https://site.financialmodelingprep.com/developer/docs/stock-market-quote-free-api/?direct=true
+func GetSymbolList(apiKey string) ([]symbol, error) {
+	url := "https://financialmodelingprep.com/api/v3/stock/list?apikey=" + apiKey
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	responseData, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var symbolList []symbol
+	err = json.Unmarshal(responseData, &symbolList)
+	if err != nil {
+		return nil, err
+	}
+	return symbolList, nil
+}
---
+++ internal/fmp/symbol_list_test.go
@@ -0,0 +1,44 @@
+package fmp_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/yoshihiro-shu/financial-bot/internal/fmp"
+)
+
+var apiKey = os.Getenv("FMP_API_KEY")
+
+func TestGetSymbolList(t *testing.T) {
+	symbolList, err := fmp.GetSymbolList(apiKey)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(symbolList) == 0 {
+		t.Error("len(symbolList) == 0")
+	}
+
+	if symbolList[0].Symbol == "" {
+		t.Error("symbolList[0].Symbol == \"\"")
+	}
+
+	if symbolList[0].Name == "" {
+		t.Error("symbolList[0].Name == \"\"")
+	}
+
+	if symbolList[0].Price == 0 {
+		t.Error("symbolList[0].Price == 0")
+	}
+
+	if symbolList[0].Ex == "" {
+		t.Error("symbolList[0].Ex == \"\"")
+	}
+
+	if symbolList[0].ExShort == "" {
+		t.Error("symbolList[0].ExShort == \"\"")
+	}
+
+	if symbolList[0].Type == "" {
+		t.Error("symbolList[0].Type == \"\"")
+	}
+}
