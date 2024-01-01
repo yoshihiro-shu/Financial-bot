d0bae09981fd61c58ca523a14aec43d6568f3c4b
yoshihiro-shu <yoihongzhu0321@gmail.com>
2024-01-02 00:57:05 +0900

    refactor: mock http req

--- repository/apache_kafka/handle_test.go
+++ repository/apache_kafka/handle_test.go
@@ -13,20 +13,49 @@ import (
 func TestHandleRequest(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
-	httpmock.RegisterResponder("GET", "http://example.com/v1/users",
-		httpmock.NewStringResponder(200, "mocked"),
-	)
-	msg := &sarama.ConsumerMessage{
-		Topic:     "request",
-		Partition: 0,
-		Offset:    0,
-		Key:       []byte(http.MethodGet),
-		Value:     []byte("http://example.com/v1/users"),
+	type args struct {
+		msg    *sarama.ConsumerMessage
+		method string
+		path   string
+		res    httpmock.Responder
 	}
-	res, err := handleRequest(msg)
-	assert.Nil(t, err)
-	defer res.Body.Close()
-	body, err := io.ReadAll(res.Body)
-	assert.Nil(t, err)
-	assert.Equal(t, "mocked", string(body))
+	type expected struct {
+		statusCode int
+		body       string
+	}
+	tests := []struct {
+		key   string
+		input args
+		expected
+	}{
+		{
+			key: "GET",
+			input: args{
+				msg: &sarama.ConsumerMessage{
+					Topic:     "request",
+					Partition: 0,
+					Offset:    0,
+					Key:       []byte(http.MethodGet),
+					Value:     []byte("http://example.com/v1/users"),
+				},
+				method: http.MethodGet,
+				path:   "http://example.com/v1/users",
+				res:    httpmock.NewStringResponder(200, http.StatusText(http.StatusOK)),
+			},
+			expected: expected{
+				statusCode: http.StatusOK,
+				body:       http.StatusText(http.StatusOK),
+			},
+		},
+	}
+	for _, tt := range tests {
+		httpmock.RegisterResponder(tt.key, tt.input.path, tt.input.res)
+		res, err := handleRequest(tt.input.msg)
+		assert.Nil(t, err)
+		defer res.Body.Close()
+		body, err := io.ReadAll(res.Body)
+		assert.Nil(t, err)
+		assert.Equal(t, tt.expected.statusCode, res.StatusCode)
+		assert.Equal(t, tt.expected.body, string(body))
+	}
 }
